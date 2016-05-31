a79021f30bb01469b4e80d98ab3aecc861465578
Petr Zemek <s3rvac@gmail.com>
2016-05-31 18:04:29 +0200

    Change links to use HTTPS instead of HTTP.

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_pwauth do
   author 'Petr Zemek'
   description 'Adds the pwauth authentication to Redmine. Requires `pwauth` to be installed.'
   version '1.0'
-  url 'http://github.com/s3rvac/redmine_pwauth'
-  author_url 'http://petrzemek.net/'
+  url 'https://github.com/s3rvac/redmine_pwauth'
+  author_url 'https://petrzemek.net/'
 end
